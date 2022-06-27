c2c03898a8fbf4639dfa3c1235fb080e5f9f4396
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-27 11:09:14 +0300

    Fixed

--- ConsoleDocker/Program.cs
+++ ConsoleDocker/Program.cs
@@ -12,7 +12,6 @@ namespace ConsoleDocker
                 Console.WriteLine("Hello World!" + i);
                 i++;
                 System.Threading.Thread.Sleep(1000);
-
             }
 
         }
