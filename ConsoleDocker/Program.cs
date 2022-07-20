70be9cfead8843016e5ce5467763520118e9cad1
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-20 16:09:16 +0300

    refactor

--- ConsoleDocker/Program.cs
+++ ConsoleDocker/Program.cs
@@ -10,7 +10,6 @@ namespace ConsoleDocker
             while (i<10000)
             {
                 Console.WriteLine("Hello World!" + i);
-                i++;
                 System.Threading.Thread.Sleep(1000);
             }
 
